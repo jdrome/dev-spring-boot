b08bbbad12325c755836e03a3b830d5327fd562d
jdrome <josedanilromero@gmail.com>
2024-12-15 13:30:13 -0600

    add properties in 06-properties-demo

--- 01-spring-boot-overview/06-properties-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ 01-spring-boot-overview/06-properties-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,10 +1,26 @@
 package com.luv2code.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    //    Inject properties for: coach.name and team.name
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    // expose a new endpoint for "teaminfo"
+
+    @GetMapping("/teaminfo")
+    public String teamInfo() {
+        return "Coach: " + coachName + ", Team: " + teamName;
+    }
 
     // expose a "/" (i.e. Root) endpoint that will return "Hello World"
 
