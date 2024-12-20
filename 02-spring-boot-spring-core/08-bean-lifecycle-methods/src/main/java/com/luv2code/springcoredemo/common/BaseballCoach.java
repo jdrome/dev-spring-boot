3a4453b8a906bd5cadb8b7a320423d456ff2bc8a
jdrome <josedanilromero@gmail.com>
2024-12-20 13:44:49 -0600

    add 02-spring-boot-spring-core/08-bean-lifecycle-methods

--- 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/luv2code/springcoredemo/common/BaseballCoach.java
+++ 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/luv2code/springcoredemo/common/BaseballCoach.java
@@ -1,17 +1,14 @@
 package com.luv2code.springcoredemo.common;
 
-import org.springframework.context.annotation.Lazy;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-public class TrackCoach implements Coach {
-    public TrackCoach() {
+public class BaseballCoach implements Coach {
+    public BaseballCoach() {
         System.out.println("In constructor: " + getClass().getSimpleName());
     }
-
     @Override
     public String getDailyWorkout() {
-        return "Run a hard 5k";
+        return "Spend 30 minutes in batting practice";
     }
 }
--- 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/luv2code/springcoredemo/common/TennisCoach.java
+++ 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/luv2code/springcoredemo/common/TennisCoach.java
@@ -1,17 +1,15 @@
 package com.luv2code.springcoredemo.common;
 
-import org.springframework.context.annotation.Lazy;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-public class TrackCoach implements Coach {
-    public TrackCoach() {
+public class TennisCoach implements Coach {
+    public TennisCoach() {
         System.out.println("In constructor: " + getClass().getSimpleName());
     }
 
     @Override
     public String getDailyWorkout() {
-        return "Run a hard 5k";
+        return "Practice your backhand volley";
     }
 }
