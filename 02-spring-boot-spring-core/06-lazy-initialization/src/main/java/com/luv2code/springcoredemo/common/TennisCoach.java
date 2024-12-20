f1738472014ce22765adb3a3ae2c9b75c6d57689
jdrome <josedanilromero@gmail.com>
2024-12-19 19:25:28 -0600

    add 02/-spring-boot-spring-core/06-lazy-initialization

--- 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/luv2code/springcoredemo/common/TennisCoach.java
+++ 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/luv2code/springcoredemo/common/TennisCoach.java
@@ -1,13 +1,11 @@
 package com.luv2code.springcoredemo.common;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
-public class TrackCoach implements Coach {
+public class TennisCoach implements Coach {
     @Override
     public String getDailyWorkout() {
-        return "Run a hard 5k";
+        return "Practice your backhand volley";
     }
 }
