c1b4d87a4685c15091bf1e8757a6bf0369ac8e72
jrom387 <jrom387@egu.edu>
2025-02-05 12:00:40 -0600

    Refactor EmployeeRestController and complete Employee Service layer

--- 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -1,4 +1,27 @@
 package com.luv2code.springboot.cruddemo.service;
 
-public class EmployeeServiceImpl {
+import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    // Add attributes
+    private EmployeeDAO employeeDAO;
+
+    // inject EmployeeDAO with constructor injection
+    @Autowired
+    public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO) {
+        employeeDAO = theEmployeeDAO;
+    }
+
+    // override appropriate methods
+    @Override
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
 }
